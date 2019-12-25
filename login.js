8235398cf76d461065d5424c27ebe1ef07226204
FarellUG <59218987+FarellUG@users.noreply.github.com>
2019-12-25 16:16:50 +0700

    Update login.js

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "vampreza" && password == "ia88"){
 alert ("Welcome, Farell");
-window.location = "Muhammad Farell 3IA88 Backup2.html"; // Redirect ke page CV
+window.location = "main.php"; // Redirect ke page CV
 return false;
 }
 else{
